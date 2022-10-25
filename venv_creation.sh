88fce9e73ceb2c09777de135372f6ee9d7c0d2be
mendrugory <iam@mendrugory.com>
2022-10-25 06:41:49 +0200

    chore: python3.10

--- venv_creation.sh
+++ venv_creation.sh
@@ -5,7 +5,7 @@ then
     rm -rf venv
 fi
 
-virtualenv  -p /usr/bin/python3.9 venv
+virtualenv  -p /usr/bin/python3 venv
 venv/bin/python -m pip install --upgrade pip
 venv/bin/pip install -r requirements.txt
 venv/bin/pip install -r requirements-dev.txt
